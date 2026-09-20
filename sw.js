8ec738daaa504a8d5d9186eb347d87b2b951026b
supremoc100-maker <supremoc100@gmail.com>
2026-09-20 19:24:03 -0400

    Refresh PWA cache for food logistics v33

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE="wilson-v32";
+const CACHE="wilson-v33";
 const CORE=[
   "/",
   "/index.html",
-  "/styles.css?v=32",
-  "/src/game.js?v=32",
+  "/styles.css?v=33",
+  "/src/game.js?v=33",
   "/manifest.webmanifest",
   "/icon.svg",
   "/vendor/phaser-3.90.0.min.js",
